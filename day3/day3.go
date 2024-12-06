c9175742c51a08f9ac928020cda2a2d807a70552
Will Shivers <tayloe93@gmail.com>
2024-12-05 21:42:04 -0600

    Add day three part two

--- day3/day3.go
+++ day3/day3.go
@@ -21,10 +21,34 @@ func main() {
 		total += multiply(string(val))
 	}
 	fmt.Println("Part 1 result: ", total)
+
+	// part 2
+	re2 := regexp.MustCompile(`(mul\((\d+),(\d+)\)|do\(\)|don't\(\))`)
+	result2 := re2.FindAll(file, -1)
+	insEnabled := true
+	total2 := 0
+
+	for _, val := range result2 {
+		stringVal := string(val)
+		if stringVal == "do()" {
+			insEnabled = true
+		}
+		if stringVal == "don't()" {
+			insEnabled = false
+		}
+
+		if insEnabled && strings.Contains(stringVal, "mul") {
+			total2 += multiply(stringVal)
+		}	
+	}
+
+	fmt.Println("Part 2 result: ", total2)
 }
 
 func multiply(instruction string) int {
-	removeInstruction := strings.ReplaceAll(instruction, "mul(", "")
+	re := regexp.MustCompile(`(?:mul\()\d+\,\d+\)`)
+	ins := re.FindString(instruction)
+	removeInstruction := strings.ReplaceAll(ins, "mul(", "")
 	removeTrailing := strings.ReplaceAll(removeInstruction, ")", "")
 	numbers := strings.Split(removeTrailing, ",")
 	num1, _ := strconv.Atoi(numbers[0])
